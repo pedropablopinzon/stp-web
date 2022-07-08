971091b6e26cd3f16e65c3f04bd754dbf4a92141
Luis Rangel Castro <luisrangelc@gmail.com>
2022-07-08 16:27:19 -0600

    fix: remove commented code

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-// import App from "./components/App";
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
